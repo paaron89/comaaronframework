9171abf4915c87f08621a9194881e311f458b7d9
Aron_Preszter <presztaron89@gmail.com>
2018-11-21 17:06:07 +0100

    Assertions added to LoginTests

--- src/test/java/pom/LoginPage.java
+++ src/test/java/pom/LoginPage.java
@@ -1,16 +1,21 @@
 package pom;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class LoginPage {
 
 
     WebDriver driver;
+    String responseText;
+
 
     @FindBy(id = "log")
     private WebElement usernameField;
@@ -22,27 +27,39 @@ public class LoginPage {
     private WebElement loginButton;
 
 
-
     //Builds webelements
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+
     }
 
 
     //Login service
-    public void login(String username, String password) {
+    public String login(String username, String password) {
         System.out.println("login service started");
 
-//TODO implement waits
         usernameField.clear();
         usernameField.sendKeys(username);
-
         passwordField.clear();
         passwordField.sendKeys(password);
         loginButton.click();
 
+//TODO implement Waits Class it's a bit messy here
+        WebElement response = driver.findElement(By.xpath("//p[@class='response']"));
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.visibilityOf(response));
+        String responseText = response.getText();
+        System.out.println("Response is: " + responseText);
 
+
+        return responseText;
+    }
+
+    public String loginText() {
+
+        System.out.println("responseText is: " + responseText);
+        return responseText;
     }
 }
--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -1,33 +1,40 @@
 package test;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pom.LoginPage;
 
 public class LoginTest extends BasicTest {
 
+
     //TestCases for Login
 
-    String loginSite = "http://store.demoqa.com/products-page/your-account/";
+    private String loginSite = "http://store.demoqa.com/products-page/your-account/";
+    private String expectedResponseText = "ERROR: The password you entered for the username username is incorrect. Lost your password?";
 
+
+    //TODO implement parameterized Test methods
 
     @Test
     public void loginTest() {
 
         LoginPage login = new LoginPage(driver);
         driver.get(loginSite);
-        login.login("username", "password");
+
+        Assert.assertEquals(login.login("username","password"),expectedResponseText);
 
 
     }
 
 
     @Test
+    //Same as loginTest1 but with other credentials
     public void loginTest2() {
 
         LoginPage login = new LoginPage(driver);
         driver.get(loginSite);
-        login.login("kacsa", "máj");
+        Assert.assertEquals(login.login("username","password"),expectedResponseText);
 
 
     }
